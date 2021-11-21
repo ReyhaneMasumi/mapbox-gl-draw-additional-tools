d32ca9ee23975d5f7a4fd02d805b1c656f0e2e02
Reyhane Masumi <41986383+ReyhaneMasumi@users.noreply.github.com>
2021-11-21 15:39:17 +0330

    fix minor bug

--- src/index.js
+++ src/index.js
@@ -216,7 +216,7 @@ class extendDrawBar {
         selectedFeatures.forEach((main) => {
             if(['Point', 'MultiPoint'].includes(main.geometry.type)) return
             console.log(main.geometry)
-            let vertex = meta.CoordAll(main.geometry);
+            let vertex = meta.coordAll(main.geometry);
             console.log(vertex)
             vertex.id = `${main.id}_vertex_${Math.floor(Math.random() * Math.floor(1000))}`;
             ids.push(vertex.id)
